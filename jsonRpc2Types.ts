568e550e13cc46b3ab813814dc1f443503885263
timonson <timo.rossa@tutanota.com>
2020-03-09 12:55:04 +0100

    -

--- jsonRpc2Types.ts
+++ jsonRpc2Types.ts
@@ -1,15 +1,6 @@
-/** A String specifying the version of the JSON-RPC protocol. MUST be exactly "2.0". */
+// https://www.jsonrpc.org/specification
 export type JsonRpcVersion = "2.0"
-
-/** Method names that begin with the word rpc followed by a period character
- * (U+002E or ASCII 46) are reserved for rpc-internal methods and extensions
- *  and MUST NOT be used for anything else. */
 export type JsonRpcReservedMethod = string
-
-/** An identifier established by the Client that MUST contain a String, Number,
- *  or NULL value if included. If it is not included it is assumed to be a
- *  notification. The value SHOULD normally not be Null and Numbers SHOULD
- *  NOT contain fractional parts [2] */
 export type JsonRpcId = number | string | null
 export type JsonRpcParams = JsonArray | JsonObject
 export type JsonRpcMethod = string

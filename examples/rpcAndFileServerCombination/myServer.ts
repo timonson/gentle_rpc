5bc50ee6268dc528140d0119918191ed8d0a0beb
timonson <timo.rossa@tutanota.com>
2020-10-30 14:53:29 +0100

    Update dependencies

--- examples/rpcAndFileServerCombination/myServer.ts
+++ examples/rpcAndFileServerCombination/myServer.ts
@@ -1,8 +1,8 @@
 import { respond as respondRpc } from "../../respond.ts";
 import { rpcMethods } from "./rpcMethods.ts";
 import { serve } from "https://deno.land/std@0.75.0/http/server.ts";
-import { fetch } from "https://cdn.jsdelivr.net/gh/timonson/salad@v0.0.7/fetch/fetchPolyfill.ts";
-import { createFileUrl } from "https://cdn.jsdelivr.net/gh/timonson/salad@v0.0.7/fetch/createFileUrl.ts";
+import { fetch } from "https://cdn.jsdelivr.net/gh/timonson/salad@v0.0.8/fetch/fetchPolyfill.ts";
+import { createFileUrl } from "https://cdn.jsdelivr.net/gh/timonson/salad@v0.0.8/fetch/createFileUrl.ts";
 
 const proto = "http";
 const addr = "0.0.0.0:8000";

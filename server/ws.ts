2f31ef34ae23e28ca477316125ac4c9609de33d1
timonson <timo.rossa@tutanota.com>
2021-01-25 13:04:31 +0100

    Improve the ws module by adding an EventListener

--- server/ws.ts
+++ server/ws.ts
@@ -1,17 +1,62 @@
 import { cleanBatch, createResponseObject } from "./creation.ts";
-import { validateRequest } from "./validation.ts";
-import {
-  makeInternalMethodsOptionsMaybe,
-  subscriptionMap,
-} from "./ws_internal_methods.ts";
+import { validateRequest, validateRpcRequestObject } from "./validation.ts";
 import { isWebSocketCloseEvent, isWebSocketPingEvent } from "./deps.ts";
 
-import type { Input } from "./ws_internal_methods.ts";
+import type { CreationInput } from "./creation.ts";
+import type { WebSocket } from "./deps.ts";
+
+export type MethodsAndIdsStore = Map<string, Set<string>>;
+type Input = Omit<CreationInput, "validationObject"> & { socket: WebSocket } & {
+  methodsAndIdsStore: MethodsAndIdsStore;
+};
+type Emission = {
+  method: string;
+  params: unknown;
+};
+
+function partialEmitListener(
+  { socket, methods, options, methodsAndIdsStore }: Input,
+) {
+  return async function emitListener(event: CustomEvent) {
+    const { method, params } = event.detail as Emission;
+    if (methodsAndIdsStore.has(method)) {
+      const ids = [...methodsAndIdsStore.get(method)!.values()];
+      return ids.map(async (id) => {
+        const response = await createResponseObject({
+          validationObject: validateRpcRequestObject(
+            { method, params, id, jsonrpc: "2.0" },
+            methods,
+          ),
+          methods,
+          options,
+        });
+        if (response) {
+          try {
+            return await socket.send(JSON.stringify(response));
+          } catch {
+            removeEventListener("emit", emitListener as EventListener);
+          }
+        }
+      });
+    }
+  };
+}
 
 export async function handleWs(
-  { socket, methods, options }: Omit<Input, "validationObject">,
+  { socket, methods, options, methodsAndIdsStore }: Input,
 ) {
   console.log("socket connected!");
+
+  const emitListener = partialEmitListener({
+    socket,
+    methods,
+    options,
+    methodsAndIdsStore,
+  });
+  if (!options.disableInternalMethods) {
+    addEventListener("emit", emitListener as EventListener);
+  }
+
   try {
     for await (const ev of socket) {
       if (typeof ev === "string") {
@@ -22,21 +67,16 @@ export async function handleWs(
             ? await cleanBatch(
               validationObjectOrBatch.map(async (validationObject) =>
                 await createResponseObject(
-                  makeInternalMethodsOptionsMaybe(
-                    { socket, validationObject, methods, options },
-                  ),
+                  { validationObject, methods, options },
                 )
               ),
             )
             : await createResponseObject(
-              makeInternalMethodsOptionsMaybe(
-                {
-                  socket,
-                  validationObject: validationObjectOrBatch,
-                  methods,
-                  options,
-                },
-              ),
+              {
+                validationObject: validationObjectOrBatch,
+                methods,
+                options,
+              },
             );
         if (responseObjectOrBatchOrNull) {
           await socket.send(JSON.stringify(responseObjectOrBatchOrNull));
@@ -47,12 +87,16 @@ export async function handleWs(
       } else if (isWebSocketCloseEvent(ev)) {
         const { code, reason } = ev;
         console.log("ws:Close", code, reason);
-        subscriptionMap.delete(socket);
+        if (!options.disableInternalMethods) {
+          removeEventListener("emit", emitListener as EventListener);
+        }
       }
     }
   } catch (err) {
     console.error(`failed to receive frame: ${err}`);
-    subscriptionMap.delete(socket);
+    if (!options.disableInternalMethods) {
+      removeEventListener("emit", emitListener as EventListener);
+    }
     if (!socket.isClosed) {
       await socket.close(1000).catch((err) => console.error(err));
     }

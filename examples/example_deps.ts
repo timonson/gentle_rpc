8d77434dfc238f635d273e0935bf2f0b914f5819
timonson <timo.rossa@tutanota.com>
2021-07-11 15:11:03 +0200

    Bump versions

--- examples/example_deps.ts
+++ examples/example_deps.ts
@@ -1,4 +1,4 @@
-export { serve } from "https://deno.land/std@0.97.0/http/server.ts";
+export { serve } from "https://deno.land/std@0.99.0/http/server.ts";
 export { default as PressF } from "https://deno.land/x/pressf@0.2.1/pressf.ts";
 export { respondWithFile } from "https://deno.land/x/pressf@0.2.1/middlewares/pressf-static/mod.ts";
 
--- tests/test_deps.ts
+++ tests/test_deps.ts
@@ -3,6 +3,6 @@ export {
   assertNotEquals,
   assertThrows,
   assertThrowsAsync,
-} from "https://deno.land/std@0.97.0/testing/asserts.ts";
+} from "https://deno.land/std@0.99.0/testing/asserts.ts";
 
 export type { ServerRequest } from "../server/deps.ts";

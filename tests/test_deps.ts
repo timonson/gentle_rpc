2d8b09ad3ddee60a2f348aaf2342762238189af1
timonson <timo.rossa@tutanota.com>
2021-09-19 20:49:16 +0200

    Push versions

--- tests/test_deps.ts
+++ tests/test_deps.ts
@@ -3,8 +3,8 @@ export {
   assertNotEquals,
   assertThrows,
   assertThrowsAsync,
-} from "https://deno.land/std@0.105.0/testing/asserts.ts";
-export { create } from "https://deno.land/x/djwt@v2.3/mod.ts";
+} from "https://deno.land/std@0.107.0/testing/asserts.ts";
+export { create } from "https://deno.land/x/djwt@v2.4/mod.ts";
 
-export type { Payload } from "https://deno.land/x/djwt@v2.3/mod.ts";
-export type { ServerRequest } from "https://deno.land/std@0.105.0/http/server.ts";
+export type { Payload } from "https://deno.land/x/djwt@v2.4/mod.ts";
+export type { ServerRequest } from "https://deno.land/std@0.107.0/http/server.ts";

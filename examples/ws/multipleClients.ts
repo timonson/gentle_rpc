d55c90904aaaec0b7184e6a2220366f294b824c4
timonson <timo.rossa@tutanota.com>
2021-09-30 03:12:23 +0200

    Update examples

--- examples/ws/multipleClients.ts
+++ examples/ws/multipleClients.ts
@@ -6,13 +6,15 @@ const remote = await createRemote(new WebSocket("ws://0.0.0.0:8000")).catch(
     throw err;
   },
 );
-
 const jwt = await remote.call("sendJwt", { user: "Bob" }) as string;
 // eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYm9iIn0.C_jTAzMzjoGGDPIK5oKg4b8Yt8nbvAmbOyAbOk_17BsNu9za8e4KD41kxRvn3RUW
+remote.socket.close();
 
 // Send messages between firstClient and secondClient:
 const firstClient = await createRemote(new WebSocket("ws://0.0.0.0:8000", jwt));
-const secondClient = await createRemote(new WebSocket("ws://0.0.0.0:8000"));
+const secondClient = await createRemote(
+  new WebSocket("ws://0.0.0.0:8000", jwt),
+);
 
 async function run(iter: AsyncGenerator<unknown>) {
   try {
@@ -24,21 +26,24 @@ async function run(iter: AsyncGenerator<unknown>) {
   }
 }
 
-// const greeting = firstClient.subscribe("sayHello");
-// const second = secondClient.subscribe("sayHello");
+const greetingFirst = firstClient.subscribe("sayHello");
+const greetingSecond = secondClient.subscribe("sayHello");
 const loginFirst = firstClient.subscribe("login");
-// const loginSecond = secondClient.subscribe("login");
+const loginSecond = secondClient.subscribe("login");
 
-// run(greeting.generator);
-// run(second.generator);
+run(greetingFirst.generator);
+run(greetingSecond.generator);
 run(loginFirst.generator);
-// run(loginSecond.generator);
-// greeting.emit(["first"]);
-// second.emitBatch([["second"], ["third"]]);
+run(loginSecond.generator);
+
+greetingFirst.emit(["first"]);
+greetingSecond.emit(["second"]);
 loginFirst.emit();
 
-// setTimeout(() => greeting.unsubscribe());
-// setTimeout(() => second.unsubscribe());
+setTimeout(() => greetingFirst.unsubscribe());
+setTimeout(() => greetingSecond.unsubscribe());
+setTimeout(() => loginFirst.unsubscribe());
+setTimeout(() => loginSecond.unsubscribe());
 
 setTimeout(() => firstClient.socket.close(), 2000);
 setTimeout(() => secondClient.socket.close(), 2000);
